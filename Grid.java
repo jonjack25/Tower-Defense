d8231681cd3d6feb54271442d1c52d8f0d0811cc
PRAN1999 <pneeldam@gmail.com>
2016-05-27 21:02:45 +0000

    Add files via upload

---
+++ Grid.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+public class Grid
+{
+    private Position[][] grid;
+    public Grid(int w, int l)
+    {
+        grid = new Position[w][l];
+        for(int i = 0; i < w; i++)
+        {
+            for(int j = 0; j < l; j++)
+            {
+                grid[w][l] = new Position(w, l, 0);
+            }
+        }
+    }
+    
+    public void setFilled(int x, int y, Entity e)
+    {
+        if(e instanceOf Turret)
+            grid[x][y].setType(1);
+    }
+    
+    public boolean isEmpty(int x, int y)
+    {
+        return grid[x][y].isEmpty();
+    }
+    
+    public void remove(int x, int y)
+    {
+        grid[x][y].setType(0);
+    }
+    
+    public void draw(Graphics g)
+    {
+        for(Position[] p : grid)
+            for(Position pos : p)
+                pos.draw(g);
+    }
+}
