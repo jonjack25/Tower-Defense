32be70e66c5b2ca3d407eaa7771eb0869e10c5f3
jonjack25 <jonjack25@users.noreply.github.com>
2016-06-12 15:27:04 -0400

    Update SlowEnemy.java

--- SlowEnemy.java
+++ SlowEnemy.java
@@ -2,40 +2,28 @@ import java.awt.Graphics;
 
 public class SlowEnemy extends Enemy
 {
+   private int health;
    private int killCost;
    private final int speed;
-   public static int HEALTH = 50;
-   private int health;
+   public static int HEALTH = 400;
 
    public SlowEnemy(Position p)
    {
        super(p);
        speed = 1000;
        setMoveTime(speed);
+   }
+   
+   public void setHealth()
+   {
        health = HEALTH;
+   }
+   
+   public void setKillCost()
+   {
        killCost = 200;
    }
-   
-   public int getHealth()
-   {
-       return health;
-   }
-   
-   public int getKillCost()
-   {
-       return killCost;
-   }
-   
-   public int getSpeed()
-   {
-       return speed;
-   }
-   
-   public void changeHealth(int damage)
-   {
-        health -= damage;
-   }
-   
+       
    public void draw(Graphics g)
    {
        
