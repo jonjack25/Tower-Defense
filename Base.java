c7b53bedeb48636becd617193eef9260a8d80cbe
PRAN1999 <pneeldam@gmail.com>
2016-06-12 14:20:11 -0400

    Add files via upload

---
+++ Base.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+public class Base extends Entity
+{
+    private Position pos;
+    private int length;
+    private int width;
+    public Base(Position p, int l, int w)
+    {
+        pos = p;
+        length = l;
+        width = w;
+    }
+
+    public void draw(Graphics g)
+    {
+        
+    }
+}
