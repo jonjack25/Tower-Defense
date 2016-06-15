3376fa6d07f9682bb0ed2436ca2abd4aaad796ee
PRAN1999 <pneeldam@gmail.com>
2016-06-15 15:37:59 -0400

    Update SlowEnemy.java

--- SlowEnemy.java
+++ SlowEnemy.java
@@ -1,3 +1,4 @@
+
 import java.awt.Graphics;
 import java.awt.*;
 import java.awt.Image;
@@ -8,7 +9,7 @@ public class SlowEnemy extends Enemy
 {
    private final int SPEED = 1000;
    public static int HEALTH = 1000;
-   private final int KILL_COST = 200;
+   private final int KILL_COST = 100;
    private final Image SLOW = new ImageIcon("slowenemy.png").getImage();
 
    public SlowEnemy(Position p)
