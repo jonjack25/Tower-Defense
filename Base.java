a2c9e3beb6dd7dc4494d7a2692214660f825605f
PRAN1999 <pneeldam@gmail.com>
2016-06-14 23:53:06 -0400

    Update Base.java

--- Base.java
+++ Base.java
@@ -1,9 +1,11 @@
 import java.awt.*;
+import javax.swing.*;
 public class Base extends Entity
 {
     private Position pos;
     private int length;
     private int width;
+    private final Image image = new ImageIcon("base.png").getImage();
     public Base(Position p, int l, int w)
     {
         pos = p;
@@ -28,6 +30,8 @@ public class Base extends Entity
 
     public void draw(Graphics g)
     {
-        
+        Graphics2D g2d = (Graphics2D)g.create();
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+        g2d.drawImage(image, getPosition().getXCoordinate(), getPosition().getYCoordinate(), (int)getPosition().getWidth() * width, (int)getPosition().getHeight() * length, null);
     }
 }
