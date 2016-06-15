6f11dc4f5ca3b6f99e864f57629c3b5100639ff1
ritheshn <rithesh.neelamagam@gmail.com>
2016-06-14 20:34:47 -0400

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,368 @@
+import java.util.*;
+import java.util.List;
+import javax.swing.Timer;
+import java.awt.*;
+import java.awt.event.*;
+public class Game
+{
+    private Grid grid;
+    private List<Turret> turrets;
+    private List<Enemy> enemies;
+    private List<Bullet> bullets;
+    private Base base;
+    private int gameState;
+    private int coins;
+    private int round;
+    private int enemyCount;
+    private int typeToSpawn;
+    private int randSpawnTime;
+    private int spawnPosition;
+    private int previousSpawn;
+    private boolean first = true;
+    private boolean next = true;
+    private Timer spawn, interval;
+
+    public Game(GameScreen gs)
+    {
+        grid = new Grid(gs);
+        turrets = new ArrayList<Turret>();
+        enemies = new ArrayList<Enemy>();
+        bullets = new ArrayList<Bullet>();
+        base = new Base(grid.getGameGrid()[22][45], 4, 5);
+        gameState = 1;
+        enemyCount = 2;
+        round = 1;
+        coins = 300;
+        randSpawnTime = (int)(Math.random() * 2001) + 500;
+        typeToSpawn = (int)(Math.random() * 3);
+        spawnPosition = (int)(Math.random() * 4) + 3;
+        previousSpawn = spawnPosition;
+        
+        spawn = new Timer(randSpawnTime, new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                if(enemyCount > 0)
+                {
+                    //System.out.println(enemyCount);
+                    while(spawnPosition == previousSpawn)
+                    {
+                        spawnPosition = (int)(Math.random() * 4) + 3;
+                    }
+                    if(typeToSpawn == 0)
+                    {
+                        Enemy en = new BaseEnemy(grid.getGameGrid()[spawnPosition][0]);
+                        enemies.add(en);
+                        grid.setFilled(grid.getGameGrid()[spawnPosition][0], en);
+                    }
+                    else if(typeToSpawn == 1)
+                    {
+                        Enemy en = new QuickEnemy(grid.getGameGrid()[spawnPosition][0]);
+                        enemies.add(en);
+                        grid.setFilled(grid.getGameGrid()[spawnPosition][0], en);
+                    }
+                    else
+                    {
+                        Enemy en = new SlowEnemy(grid.getGameGrid()[spawnPosition][0]);
+                        enemies.add(en);
+                        grid.setFilled(grid.getGameGrid()[spawnPosition][0], en);
+                    }
+                      
+                    randSpawnTime = (int)(Math.random() * 2001) + 2000;
+                    //setDelay(randSpawnTime);
+                    next = true;
+                    typeToSpawn = (int)(Math.random() * 3);
+                    previousSpawn = spawnPosition;
+                    spawnPosition = (int)(Math.random() * 4) + 3;
+                    enemyCount--;
+                    //System.out.println(enemies.size());
+                }
+            }
+        });
+        interval = new Timer(3000, new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                gameState = 1;
+                if(enemyCount == 0)
+                {
+                    round++;
+                    scaleEnemies();
+                }
+            }
+        });
+        spawn.setInitialDelay(0);
+        spawn.start();
+        
+        turrets.add(new StrongTurret(grid.getGameGrid()[9][12]));
+    }
+    
+    public Grid getGrid()
+    {
+        return grid;
+    }
+    
+    public void updateGame()
+    {
+        if(gameState == 0)
+        {
+            if(first == true)
+            {
+                interval.start();
+                first = false;
+            }
+        }
+        else if(gameState == 1)
+        {
+            if(first == false)
+            {
+                first = true;
+                interval.stop();
+                spawn.start();
+            }
+            fireTurrets();
+            moveEnemies();
+            moveBullets();
+            collideEnemiesAndBullets();
+            removeEntities();
+            if(enemies.size() == 0)
+            {
+                gameState = 0;
+            }
+            if(baseHasBeenBreached())
+            {
+                gameState = 2;
+                System.out.println("Base has been breached");
+            }
+        }
+        else
+        {
+            interval.stop();
+            spawn.stop();
+        }
+    }
+    
+    public void draw(Graphics g)
+    {
+        grid.draw(g);
+        for(Turret t : turrets)
+            t.draw(g);
+        for(Enemy e: enemies)
+            e.draw(g);
+        for(Bullet b : bullets)
+            b.draw(g);
+        base.draw(g);
+    }
+    
+    public Turret purchaseStrongTurret()
+    {
+        if(coins >= StrongTurret.PURCHASE_COST)
+        {
+            coins -= StrongTurret.PURCHASE_COST;
+            return new StrongTurret();
+        }
+        return null;
+    }
+    
+    public Turret purchaseFastTurret()
+    {
+        if(coins >= FastTurret.PURCHASE_COST)
+        {
+            coins -= FastTurret.PURCHASE_COST;
+            return new FastTurret();
+        }
+        return null;
+    }
+    
+    public Turret purchaseBaseTurret()
+    {
+        if(coins >= BaseTurret.PURCHASE_COST)
+        {
+            coins -= BaseTurret.PURCHASE_COST;
+            return new BaseTurret();
+        }
+        return null;
+    }
+    
+    public void cancelPurchase(Turret t)
+    {
+        if(t instanceof StrongTurret)
+            coins += StrongTurret.PURCHASE_COST;
+        else if(t instanceof FastTurret)
+            coins += FastTurret.PURCHASE_COST;
+        else if(t instanceof BaseTurret)
+            coins += BaseTurret.PURCHASE_COST;
+    }
+    
+    public void placeTurret(Turret t, Position p)
+    {
+        turrets.add(t);
+        t.setPosition(p);
+        int x = p.getRow();
+        int y = p.getCol();
+        for(int i = x; i > x - 3; i--)
+        {
+            for(int j = y; j > y - 3; j--)
+            {
+                grid.setFilled(grid.getGameGrid()[i][j], t);
+            }
+        }
+    }
+    
+    public void upgradeTurret(Turret t)
+    {
+        if(coins >= t.getUpgradeCost())
+        {
+            coins -= t.getUpgradeCost();
+            t.upgradeDamage();
+        }
+    }
+    
+    public void getKillCoins(Enemy e)
+    {
+        coins += e.getKillCost();
+    }
+    
+    public void scaleEnemies()
+    {
+        enemyCount = 2 * round;
+        QuickEnemy.HEALTH += 5;
+        BaseEnemy.HEALTH += 10;
+        SlowEnemy.HEALTH += 15;
+    }
+    
+    public void fireTurrets()
+    {
+        for(Turret t : turrets)
+        {
+            Bullet b = t.fire(enemies);
+            if(b != null)
+            {
+                b.setPosition(t.getPosition());
+                bullets.add(b);
+            }
+        }
+    }
+    
+    public void moveBullets()
+    {
+        Position temp;
+        for(Bullet b : bullets)
+        {
+            temp = b.move();
+            if(temp == null)
+                continue;
+            else
+            {
+                b.setPosition(temp);
+                grid.setFilled(temp, b);
+            }
+        }
+    }
+    
+    public void moveEnemies()
+    {
+        Point temp;
+        int axis, xTarget, yTarget;
+        for(Enemy e : enemies)
+        {
+            temp = e.move();
+            if(temp == null)
+                continue;
+            else
+            {
+                axis = e.getPath().getAxis();
+                xTarget = (int)temp.getX();
+                yTarget = (int)temp.getY();
+                if(grid.getGameGrid()[xTarget][yTarget].isEmpty() && grid.isInPath(grid.getGameGrid()[xTarget][yTarget]))
+                {
+                    grid.setFilled(e.getPosition(), null);
+                    e.setPosition(grid.getGameGrid()[xTarget][yTarget]);
+                    grid.setFilled(grid.getGameGrid()[xTarget][yTarget], e);
+                }
+                else if(axis == 1)
+                {
+                    if(grid.getGameGrid()[xTarget + 1][yTarget].isEmpty() && grid.isInPath(grid.getGameGrid()[xTarget + 1][yTarget]))
+                    {
+                        grid.setFilled(e.getPosition(), null);
+                        e.setPosition(grid.getGameGrid()[xTarget + 1][yTarget]);
+                        grid.setFilled(grid.getGameGrid()[xTarget + 1][yTarget], e);
+                    }
+                    else if(grid.getGameGrid()[xTarget - 1][yTarget].isEmpty() && grid.isInPath(grid.getGameGrid()[xTarget - 1][yTarget]))
+                    {
+                        grid.setFilled(e.getPosition(), null);
+                        e.setPosition(grid.getGameGrid()[xTarget - 1][yTarget]);
+                        grid.setFilled(grid.getGameGrid()[xTarget - 1][yTarget], e);
+                    }
+                }
+                else if(axis == -1)
+                {
+                    if(grid.getGameGrid()[xTarget][yTarget + 1].isEmpty() && grid.isInPath(grid.getGameGrid()[xTarget][yTarget + 1]))
+                    {
+                        grid.setFilled(e.getPosition(), null);
+                        e.setPosition(grid.getGameGrid()[xTarget][yTarget + 1]);
+                        grid.setFilled(grid.getGameGrid()[xTarget][yTarget + 1], e);
+                    }
+                    else if(grid.getGameGrid()[xTarget][yTarget - 1].isEmpty() && grid.isInPath(grid.getGameGrid()[xTarget][yTarget - 1]))
+                    {
+                        grid.setFilled(e.getPosition(), null);
+                        e.setPosition(grid.getGameGrid()[xTarget][yTarget - 1]);
+                        grid.setFilled(grid.getGameGrid()[xTarget][yTarget - 1], e);
+                    }
+                }
+            }
+        }
+    }
+    
+    public void collideEnemiesAndBullets()
+    {
+        for(Bullet b : bullets)
+        {
+            for(Enemy e : enemies)
+            {
+                if(b.hasCollided(e))
+                {
+                    b.damageEnemy(e);
+                    b.changeGameState();
+                }
+            }
+        }
+    }
+    
+    public void removeEntities()
+    {
+        for(int i = bullets.size() - 1; i >= 0; i--)
+        {
+            if(bullets.get(i).getGameState() == 1)
+            {
+                bullets.remove(i);
+            }
+        }
+        for(int i = enemies.size() - 1; i >= 0; i--)
+        {
+            if(enemies.get(i).getHealth() <= 0)
+            {
+                coins += enemies.get(i).getKillCost();
+                grid.setFilled(enemies.get(i).getPosition(), null);
+                enemies.remove(i);
+            }
+        }
+    }
+    
+    public boolean baseHasBeenBreached()
+    {
+        for(Enemy e : enemies)
+        {
+            for(int i = base.getPosition().getRow(); i < base.getPosition().getRow() + base.getLength(); i++)
+            {
+                for(int j = base.getPosition().getCol(); j < base.getPosition().getCol() + base.getWidth(); j++)
+                {
+                    if(e.getPosition().equals(grid.getGameGrid()[i][j]))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
---
+++ Turret.java
@@ -0,0 +1,124 @@
+import java.awt.Graphics;
+import java.util.List;
+import java.awt.Point;
+import javax.swing.Timer;
+import java.util.TimerTask;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+import javax.swing.*;
+
+public class Turret extends Entity
+{
+    private int radius = 150;
+    private boolean canFire = true;
+    private Position pos;
+    private Timer reload;
+    private Enemy fireAt;
+    private int upgrade;
+    private int upgradeCost = 50;
+    private int damage;
+    private int speed;
+    private Image image;
+    
+    public Turret()
+    {
+        
+    }
+
+    public Turret(Position p)
+    {
+        pos = p;
+    }
+    
+    public int getReloadSpeed()
+    {
+        return speed;
+    }
+    
+    public void draw(Graphics g)
+    {
+        Graphics2D g2d = (Graphics2D)g.create();
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+        g2d.drawImage(image, getPosition().getXCoordinate() - (int)getPosition().getWidth() * 2, getPosition().getYCoordinate() - (int)getPosition().getWidth() * 2, (int)getPosition().getWidth() * 3, (int)getPosition().getHeight() * 3, null);
+    }
+    
+    public void setImage(Image i)
+    {
+        image = i;
+    }
+    
+    public int getDamage()
+    {
+        return damage;
+    }
+    
+    public void setDamage(int d)
+    {
+        damage = d;
+    }
+    
+    public void setUpgrade(int u)
+    {
+        upgrade = u;
+    }
+    
+    public void setPosition(Position p)
+    {
+        pos = p;
+    }
+    
+    public int getUpgradeCost()
+    {
+        return upgradeCost;
+    }
+    
+    public Position getPosition()
+    {
+        return pos;
+    }
+    
+    public int getRadius()
+    {
+        return radius;
+    }
+    
+    public void upgradeDamage()
+    {
+        damage += upgrade;
+        upgradeCost = (int)(upgradeCost * 1.5);
+    }
+    
+    public void setTimer(int s)
+    {
+        speed = s;
+        reload = new Timer(s, new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                canFire = true;
+            }
+        });
+    }
+    
+    public Bullet fire(List<Enemy> e)
+    {
+        double distance = 0;
+        int random = (int)(Math.random());
+        if(canFire)
+        {
+            for(int i = 0; i < e.size(); i++)
+            {
+                distance = Math.sqrt(Math.pow(e.get(i).getXCoordinate() - pos.getXCoordinate(), 2) + Math.pow(e.get(i).getYCoordinate() - pos.getYCoordinate(), 2));        
+                if(radius >= distance)
+                {
+                    Bullet b = new Bullet(e.get(i), damage);
+                    canFire = false;
+                    reload.restart();
+                    return b;
+                }
+            }
+        }
+        return null;
+    }
+}
