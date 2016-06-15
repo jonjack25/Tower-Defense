0d92e64a308a0c6d52fbe3c839ba4fa3cbcb7f8c
PRAN1999 <pneeldam@gmail.com>
2016-06-15 14:17:11 -0400

    Update GamePath.java

--- GamePath.java
+++ GamePath.java
@@ -6,10 +6,12 @@ public class GamePath
     public static int[] path = new int[]{16, 15, 8, 22, 28, 11, 31, 8, 37, 8, 40, 22, 49};;
     public static int[] axis = new int[]{1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1};
     public static int[] directions = new int[]{1, 1, -1, 1, 1, -1, 1, -1, 1, 1, 1, 1, 1};
+    public static int PIXELS = 4;
     private int axisPosition;
     private int dirPosition;
     private int pathWidth;
     private int posInPath;
+    private boolean turn = false;
     public GamePath()
     {
         axisPosition = 0;
@@ -20,6 +22,7 @@ public class GamePath
     
     public Point getNextMove(Position p)
     {
+        turn = false;
         int xTarget = -1, yTarget = -1, targetExtend;
         if(axis[axisPosition] == 1)
         {
@@ -38,6 +41,7 @@ public class GamePath
                     posInPath++;
                     yTarget = p.getCol();
                     xTarget = p.getRow();
+                    turn = true;
                 }
             }
             else if(directions[dirPosition] == -1)
@@ -54,6 +58,7 @@ public class GamePath
                     posInPath++;
                     yTarget = p.getCol();
                     xTarget = p.getRow();
+                    turn = true;
                 }
             }
         }
@@ -74,6 +79,7 @@ public class GamePath
                     posInPath++;
                     yTarget = p.getCol();
                     xTarget = p.getRow();
+                    turn = true;
                 }
             }
             else if(directions[dirPosition] == -1)
@@ -90,9 +96,82 @@ public class GamePath
                     posInPath++;
                     yTarget = p.getCol();
                     xTarget = p.getRow();
+                    turn = true;
                 }
             }
         }
+//         if(axis[axisPosition] == -1)
+//         {
+//             targetExtend = (int)(Math.random() * pathWidth * Grid.POSITION_SIZE);
+//             if(directions[dirPosition] == 1)
+//             {
+//                 if(p.getYCoord() < path[posInPath] * Grid.POSITION_SIZE + targetExtend)
+//                 {
+//                     yTarget = p.getYCoord() + directions[dirPosition] * PIXELS;
+//                     xTarget = p.getXCoord();
+//                 }
+//                 else
+//                 {
+//                     axisPosition++;
+//                     dirPosition++;
+//                     posInPath++;
+//                     yTarget = p.getYCoord();
+//                     xTarget = p.getXCoord();
+//                 }
+//             }
+//             else if(directions[dirPosition] == -1)
+//             {
+//                 if(p.getYCoord() > path[posInPath] * Grid.POSITION_SIZE - targetExtend)
+//                 {
+//                     yTarget = p.getYCoord() + directions[dirPosition] * PIXELS;
+//                     xTarget = p.getXCoord();
+//                 }
+//                 else
+//                 {
+//                     axisPosition++;
+//                     dirPosition++;
+//                     posInPath++;
+//                     yTarget = p.getYCoord();
+//                     xTarget = p.getXCoord();
+//                 }
+//             }
+//         }
+//         else if(axis[axisPosition] == 1)
+//         {
+//             targetExtend = (int)(Math.random() * pathWidth * Grid.POSITION_SIZE);
+//             if(directions[dirPosition] == 1)
+//             {
+//                 if(p.getXCoord() < path[posInPath] * Grid.POSITION_SIZE + targetExtend)
+//                 {
+//                     yTarget = p.getYCoord();
+//                     xTarget = p.getXCoord() + directions[dirPosition] * PIXELS;
+//                 }
+//                 else
+//                 {
+//                     axisPosition++;
+//                     dirPosition++;
+//                     posInPath++;
+//                     yTarget = p.getYCoord();
+//                     xTarget = p.getXCoord();
+//                 }
+//             }
+//             else if(directions[dirPosition] == -1)
+//             {
+//                 if(p.getXCoord() > path[posInPath] * Grid.POSITION_SIZE - targetExtend)
+//                 {
+//                     yTarget = p.getYCoord();
+//                     xTarget = p.getXCoord() + directions[dirPosition] * PIXELS;
+//                 }
+//                 else
+//                 {
+//                     axisPosition++;
+//                     dirPosition++;
+//                     posInPath++;
+//                     yTarget = p.getYCoord();
+//                     xTarget = p.getXCoord();
+//                 }
+//             }
+//         }
         return new Point(xTarget, yTarget);
     }
     
@@ -104,6 +183,11 @@ public class GamePath
     public int getDirection()
     {
         return directions[dirPosition];
+    }
+    
+    public boolean getTurn()
+    {
+        return turn;
     }
     
     public int oscillate(int p)
