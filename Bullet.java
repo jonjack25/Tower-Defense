9e9be909396f1cf2020f6c9b5097828322b09520
jonjack25 <jonjack25@users.noreply.github.com>
2016-06-14 23:16:30 -0400

    Update Bullet.java

--- Bullet.java
+++ Bullet.java
@@ -6,17 +6,18 @@ import javax.swing.*;
 public class Bullet extends Entity
 {
     private Enemy e;
-    private Position pos;
-    private final int speed = 4;
+    private Point p = new Point();
     private int damage;
     private int gameState = 0;
+    private final int PIXELS = 4;
+    private Position pos;
     Image bullet;
     
     public Bullet(Enemy enemy, int d)
     {
         e = enemy;
         damage = d;
-        bullet = new ImageIcon("bullet.png").getImage();
+        bullet = new ImageIcon("newBullet.png").getImage();
     }
     
     public void changeGameState()
@@ -32,28 +33,63 @@ public class Bullet extends Entity
     public void setPosition(Position p)
     {
         pos = p;
+        setPoint(pos);
+    }
+    
+    public void setPoint(Position pos)
+    {
+        p.setLocation(new Point(pos.getXCoordinate(), pos.getYCoordinate()));
+    }
+    
+    public void moveX(int num)
+    {
+       p.translate(num, 0);
+    }
+
+    public void moveY(int num)
+    {
+       p.translate(0, num);
     }
     
     public Position getPosition()
     {
         return pos;
     }
-            
-    public Position move()
+ 
+    public void move()
     {
-        return e.getPosition();
+        int slopeX = (int)(e.getXCoordinate() - p.getX());
+        int slopeY = (int)(e.getYCoordinate() - p.getY());
+ 
+        if(slopeX > 0)
+        {
+            moveX(PIXELS);
+        }
+        else if(slopeX < 0)
+        {
+            moveX(-1 * PIXELS);
+        }
+        
+        if(slopeY > 0)
+        {
+            moveY(PIXELS);
+        }
+        else if(slopeY < 0)
+        {
+            moveY(-1 * PIXELS);
+        }
     }
     
     public void draw(Graphics g)
     {
         Graphics2D g2d = (Graphics2D)g.create();
         g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
-        g2d.drawImage(bullet, getPosition().getXCoordinate(), getPosition().getYCoordinate(), (int)getPosition().getWidth(), (int)getPosition().getHeight(), null);
+        g2d.drawImage(bullet, (int)p.getX(), (int)p.getY(), (int)getPosition().getWidth(), (int)getPosition().getHeight(), null);
     }
     
     public boolean hasCollided(Enemy e)
     {
-        if(pos.equals(e.getPosition()))
+        if(p.getX() == e.getXCoordinate() && p.getY() == (e.getYCoordinate()))
         {
             return true;
         }
