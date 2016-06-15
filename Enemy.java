a6dc38f51b8f7d075e646df5a9e729ef94117247
PRAN1999 <pneeldam@gmail.com>
2016-06-15 14:15:35 -0400

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -15,18 +15,21 @@ public class Enemy extends Entity
     private int speed;
     private GamePath path;
     private Image image;
+    private int xCoord, yCoord;
     
     public Enemy(Position p)
     {
         pos = p;
         path = new GamePath();
+        xCoord = p.getXCoordinate();
+        yCoord = p.getYCoordinate();
     }
     
     public void draw(Graphics g)
     {
         Graphics2D g2d = (Graphics2D)g.create();
         g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
-        g2d.drawImage(image, getPosition().getXCoordinate(), getPosition().getYCoordinate(), (int)getPosition().getWidth(), (int)getPosition().getHeight(), null);
+        g2d.drawImage(image, getXCoord(), getYCoord(), (int)getPosition().getWidth(), (int)getPosition().getHeight(), null);
     }
     
     public int getHealth()
@@ -37,6 +40,22 @@ public class Enemy extends Entity
     public int getKillCost()
     {
         return killCost;
+    }
+    
+    public int getXCoord()
+    {
+        return xCoord;
+    }
+    
+    public int getYCoord()
+    {
+        return yCoord;
+    }
+    
+    public void setCoordinates(Point p)
+    {
+        xCoord = (int)p.getX();
+        yCoord = (int)p.getY();
     }
     
     public int getSpeed()
@@ -57,6 +76,7 @@ public class Enemy extends Entity
     public void setPosition(Position p)
     {
         pos = p;
+        setCoordinates(new Point(p.getXCoordinate(), p.getYCoordinate()));
     }
     
     public void setHealth(int h)
