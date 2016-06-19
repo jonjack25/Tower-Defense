74bcd2c2f153f8e8c4c0535a5bb7b67b64d74350
jonjack25 <jonjack25@users.noreply.github.com>
2016-06-19 14:37:08 -0400

    Update SlowEnemy.java

--- SlowEnemy.java
+++ SlowEnemy.java
@@ -1,14 +1,17 @@
+//SlowEnemy - Initializes the values and timer for a SlowEnemy
+
 import java.awt.*;
 import javax.swing.*;
 
 public class SlowEnemy extends Enemy
 {
-   //CONSTANTS
+   //Constants
    private final int SPEED = 500;
    public static int HEALTH = 500;
-   public static int KILL_COST = 35;
+   private final int KILL_COST = 35;
    private final Image SLOW = new ImageIcon("resources/slowenemy.png").getImage();
 
+   //Constructors
    public SlowEnemy(Position p)
    {
        super(p);
