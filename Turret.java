0e7da16a09a389fa816b29e99b339550e02ba581
jonjack25 <jonjack25@users.noreply.github.com>
2016-06-19 14:37:41 -0400

    Update Turret.java

--- Turret.java
+++ Turret.java
@@ -1,28 +1,25 @@
-import java.awt.Graphics;
-import java.util.List;
-import java.awt.Point;
-import javax.swing.Timer;
-import java.util.TimerTask;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+//Turret - The Turret Class creates and draws Turrets used in the game
+
 import java.awt.*;
-import java.awt.image.*;
-import javax.swing.*;
-import java.awt.geom.*;
+import java.awt.event.*;
+import java.util.List;
+import javax.swing.Timer;
 
 public class Turret extends Entity
 {
-    private int radius = 150;
-    private boolean canFire = true;
-    private Position pos;
-    private Timer reload;
-    private Enemy fireAt;
-    private int upgrade;
-    private int upgradeCost = 50;
-    private int damage;
+    //Private Fields
     private int speed;
+    private int damage;
+    private int upgrade;
+    private int radius = 150;
+    private int upgradeCost = 50;
+    private boolean canFire = true;
     private Image image;
+    private Enemy fireAt;
+    private Timer reload;    
+    private Position pos;
     
+    //Constructors
     public Turret()
     {
         
@@ -33,31 +30,10 @@ public class Turret extends Entity
         pos = p;
     }
     
-    public int getReloadSpeed()
+    //Mutators
+    public void setDamage(int d)
     {
-        return speed;
-    }
-    
-    public void draw(Graphics g)
-    {
-        Graphics2D g2d = (Graphics2D)g.create();
-        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
-        g2d.rotate(getAngle() - 165, pos.getXCoordinate() - pos.getWidth() / 2, pos.getYCoordinate() - pos.getWidth() / 2);
-        g2d.drawImage(image, getPosition().getXCoordinate() - (int)getPosition().getWidth() * 2, getPosition().getYCoordinate() - (int)getPosition().getWidth() * 2, (int)getPosition().getWidth() * 3, (int)getPosition().getHeight() * 3, null);
-    }
-    
-    private double getAngle()
-    {
-        if(fireAt == null)
-            return 0;
-        else
-        {
-            double x1 = pos.getXCoordinate() - pos.getWidth() / 2, 
-                   y1 = pos.getYCoordinate() - pos.getWidth() / 2;
-            double x2 = fireAt.getPosition().getXCoordinate() + fireAt.getPosition().getWidth() / 2,
-                   y2 = fireAt.getPosition().getYCoordinate() + fireAt.getPosition().getWidth() / 2;
-            return Math.atan2(y2 - y1, x2 - x1);
-        }
+        damage = d;
     }
     
     public void setImage(Image i)
@@ -65,50 +41,9 @@ public class Turret extends Entity
         image = i;
     }
     
-    public int getDamage()
-    {
-        return damage;
-    }
-    
-    public void setDamage(int d)
-    {
-        damage = d;
-    }
-    
-    public void setUpgrade(int u)
-    {
-        upgrade = u;
-    }
-    
-    public int getUpgrade()
-    {
-        return upgrade;
-    }
-    
     public void setPosition(Position p)
     {
         pos = p;
-    }
-    
-    public int getUpgradeCost()
-    {
-        return upgradeCost;
-    }
-    
-    public Position getPosition()
-    {
-        return pos;
-    }
-    
-    public int getRadius()
-    {
-        return radius;
-    }
-    
-    public void upgradeDamage()
-    {
-        damage += upgrade;
-        upgradeCost = (int)(upgradeCost * 1.5);
     }
     
     public void setTimer(int s)
@@ -123,10 +58,72 @@ public class Turret extends Entity
         });
     }
     
+    public void setUpgrade(int u)
+    {
+        upgrade = u;
+    }
+    
+    //Accessors
+    private double getAngle()
+    {
+        if(fireAt == null)
+            return 0;
+        else
+        {
+            double x1 = pos.getXCoordinate() - pos.getWidth() / 2, 
+                   y1 = pos.getYCoordinate() - pos.getWidth() / 2;
+            double x2 = fireAt.getPosition().getXCoordinate() + fireAt.getPosition().getWidth() / 2,
+                   y2 = fireAt.getPosition().getYCoordinate() + fireAt.getPosition().getWidth() / 2;
+            return Math.atan2(y2 - y1, x2 - x1);
+        }
+    }
+    
+    public int getDamage()
+    {
+        return damage;
+    }
+    
+    public Position getPosition()
+    {
+        return pos;
+    }
+    
+    public int getRadius()
+    {
+        return radius;
+    }
+    
+    public int getReloadSpeed()
+    {
+        return speed;
+    }
+    
+    public int getUpgrade()
+    {
+        return upgrade;
+    }
+    
+    public int getUpgradeCost()
+    {
+        return upgradeCost;
+    }
+    
+    //Methods
+    
+    //Postcondition - Turret is drawn and rotated at an angle towards the Enemy
+    public void draw(Graphics g)
+    {
+        Graphics2D g2d = (Graphics2D)g.create();
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+        g2d.rotate(getAngle() - 165, pos.getXCoordinate() - pos.getWidth() / 2, pos.getYCoordinate() - pos.getWidth() / 2);
+        g2d.drawImage(image, getPosition().getXCoordinate() - (int)getPosition().getWidth() * 2, getPosition().getYCoordinate() - (int)getPosition().getWidth() * 2, (int)getPosition().getWidth() * 3, (int)getPosition().getHeight() * 3, null);
+    }
+    
+    //Postcondition - Locates an Enemy within the Turret's radius and creates a new Bullet to fire at a specified Enemy
+    //                The reload Timer is reset
     public Bullet fire(List<Enemy> e)
     {
         double distance = 0;
-        int random = (int)(Math.random());
         if(canFire)
         {
             for(int i = 0; i < e.size(); i++)
@@ -143,5 +140,12 @@ public class Turret extends Entity
             }
         }
         return null;
+    }
+    
+    //Postcondition - The damage of the Turret is upgraded and the cost to upgrade increases
+    public void upgradeDamage()
+    {
+        damage += upgrade;
+        upgradeCost = (int)(upgradeCost * 1.5);
     }
 }
