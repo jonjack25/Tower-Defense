ba9b44f0544428ec8cb243902a3f0655bd16fa9b
jonjack25 <jonjack25@users.noreply.github.com>
2016-06-19 14:35:46 -0400

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,24 +1,25 @@
-import java.awt.Graphics;
-import javax.swing.*;
-import javax.swing.Timer;
+//Enemy - The Enemy Class creates a draws the enemies for the game
+
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Enemy extends Entity
 {
-    private boolean canMove = true;
-    private Position pos;
-    private Timer moving;
+    //Private Fields
+    private int speed;
+    private int xCoord;
+    private int yCoord;  
     private int health;
     private int killCost;
-    private int speed;
-    private GamePath path;
+    private int fullHealth;    
+    private boolean canMove = true;
     private Image image;
-    private int xCoord, yCoord;
+    private Timer moving;
+    private Position pos;
+    private GamePath path;
     
-    private int fullHealth;
-    
+    //Constructors
     public Enemy(Position p)
     {
         pos = p;
@@ -27,6 +28,103 @@ public class Enemy extends Entity
         yCoord = p.getYCoordinate();
     }
     
+    //Mutators
+    public void setCoordinates(Point p)
+    {
+        xCoord = (int)p.getX();
+        yCoord = (int)p.getY();
+    }
+    
+    public void setHealth(int h)
+    {
+        health = h;
+        fullHealth = h;
+    }
+    
+    public void setImage(Image i)
+    {
+        image = i;
+    }
+    
+    public void setKillCost(int k)
+    {
+        killCost = k;
+    }
+    
+    public void setMoveTime(int speed)
+    {
+        moving = new Timer(speed, new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                canMove = true;
+            }
+        });
+        moving.setInitialDelay(0);
+        moving.start();
+    }
+    
+    public void setPosition(Position p)
+    {
+        pos = p;
+        setCoordinates(new Point(p.getXCoordinate(), p.getYCoordinate()));
+    }
+    
+    //Accessors
+    public int getHealth()
+    {
+        return health;
+    }
+    
+    public int getKillCost()
+    {
+        return killCost;
+    }
+    
+    public GamePath getPath()
+    {
+        return path;
+    }
+    
+    public Position getPosition()
+    {
+        return pos;
+    }
+    
+    public int getSpeed()
+    {
+        return speed;
+    }
+    
+    public int getXCoord()
+    {
+        return xCoord;
+    }
+        
+    public int getXCoordinate()
+    {
+        return pos.getXCoordinate();
+    }
+    
+    public int getYCoord()
+    {
+        return yCoord;
+    }
+    
+    public int getYCoordinate()
+    {
+        return pos.getYCoordinate();
+    }
+    
+    //Methods
+    
+    //Postcondition - Adjusts the Enemy's health based on the Turret's damage
+    public void changeHealth(int damage)
+    {
+        health -= damage;
+    }
+    
+    //Postcondition - Enemy is drawn on the path
     public void draw(Graphics g)
     {
         Graphics2D g2d = (Graphics2D)g.create();
@@ -44,105 +142,12 @@ public class Enemy extends Entity
             g.drawRect(getXCoord() + 0, getYCoord() - 5 - 15, 25, 5);
             g.setColor(new Color(255, 0, 0));
             g.fillRect(getXCoord() + 0, getYCoord() - 5 - 15, 25, 5);
-            //g.fillRect(getXCoord() + 0, getYCoord() - 5, 25 * (int)(health / fullHealth), 5);
-            //g.setColor(new Color(255, 0, 0));
-            //g.fillRect(getXCoord() + 25 * (int)(health / fullHealth), getYCoord() - 5, 25 - 25 * (int)(health / fullHealth), 5);
             g.setColor(new Color(0, 255, 0));
             g.fillRect(getXCoord() + 0, getYCoord() - 5 - 15, 25 * health / fullHealth, 5);
         }
     }
     
-    public int getHealth()
-    {
-        return health;
-    }
-    
-    public int getKillCost()
-    {
-        return killCost;
-    }
-    
-    public int getXCoord()
-    {
-        return xCoord;
-    }
-    
-    public int getYCoord()
-    {
-        return yCoord;
-    }
-    
-    public void setCoordinates(Point p)
-    {
-        xCoord = (int)p.getX();
-        yCoord = (int)p.getY();
-    }
-    
-    public int getSpeed()
-    {
-        return speed;
-    }
-    
-    public void setImage(Image i)
-    {
-        image = i;
-    }
-    
-    public void setKillCost(int k)
-    {
-        killCost = k;
-    }
-    
-    public void setPosition(Position p)
-    {
-        pos = p;
-        setCoordinates(new Point(p.getXCoordinate(), p.getYCoordinate()));
-    }
-    
-    public void setHealth(int h)
-    {
-        health = h;
-        fullHealth = h;
-    }
-    
-    public Position getPosition()
-    {
-        return pos;
-    }
-    
-    public GamePath getPath()
-    {
-        return path;
-    }
-        
-    public int getXCoordinate()
-    {
-        return pos.getXCoordinate();
-    }
-    
-    public int getYCoordinate()
-    {
-        return pos.getYCoordinate();
-    }
-    
-    public void changeHealth(int damage)
-    {
-        health -= damage;
-    }
-    
-    public void setMoveTime(int speed)
-    {
-        moving = new Timer(speed, new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                canMove = true;
-            }
-        });
-        moving.setInitialDelay(0);
-        moving.start();
-    }
-    
+    //Postcondition - Moves the Enemy to the next point on the path
     public Point move()
     {
         if(canMove)
