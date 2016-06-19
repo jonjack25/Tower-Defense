19abae0ca3fa54028123b8821fc3113d0fd72d93
jonjack25 <jonjack25@users.noreply.github.com>
2016-06-19 14:35:24 -0400

    Update Bullet.java

--- Bullet.java
+++ Bullet.java
@@ -1,18 +1,22 @@
-import java.awt.Color;
-import java.awt.Graphics;
+//Bullet - The Bullet Class creates, draws and moves the projectile Turrets shoot
+
 import java.awt.*;
 import javax.swing.*;
 
 public class Bullet extends Entity
 {
-    private Enemy e;
-    private Point p = new Point();
+    //Private Fields
     private int damage;
     private int gameState = 0;
-    private final int PIXELS = 12;
+    private Enemy e;
     private Position pos;
     private Image bullet;
+    private Point p = new Point();
+
+    //Constants
+    private final int PIXELS = 12;    
     
+    //Constructors
     public Bullet(Enemy enemy, int d)
     {
         e = enemy;
@@ -20,19 +24,15 @@ public class Bullet extends Entity
         bullet = new ImageIcon("resources/newBullet.png").getImage();
     }
     
+    //Mutators    
     public void changeGameState()
     {
         gameState = 1;
     }
     
-    public int getGameState()
+    public void setPoint(Position pos)
     {
-        return gameState;
-    }
-    
-    public Enemy getEnemy()
-    {
-        return e;
+        p.setLocation(new Point(pos.getXCoordinate() - Grid.POSITION_SIZE / 2, pos.getYCoordinate() - Grid.POSITION_SIZE / 2));
     }
     
     public void setPosition(Position p)
@@ -41,26 +41,49 @@ public class Bullet extends Entity
         setPoint(pos);
     }
     
-    public void setPoint(Position pos)
+    //Accessors
+    public Enemy getEnemy()
     {
-        p.setLocation(new Point(pos.getXCoordinate() - Grid.POSITION_SIZE / 2, pos.getYCoordinate() - Grid.POSITION_SIZE / 2));
+        return e;
     }
     
-    public void moveX(int num)
+    public int getGameState()
     {
-       p.translate(num, 0);
-    }
-
-    public void moveY(int num)
-    {
-       p.translate(0, num);
+        return gameState;
     }
     
     public Position getPosition()
     {
         return pos;
     }
- 
+    
+    //Methods
+    
+    //Postcondition - Takes the Enemy and changes its health based on the Bullet's damage
+    //                Changes the gameState to 1
+    public void damageEnemy(Enemy enemy)
+    {
+        enemy.changeHealth(damage);
+        changeGameState();
+    }
+    
+    //Postcondition - Draws the bullet on the grid    
+    public void draw(Graphics g)
+    {
+        Graphics2D g2d = (Graphics2D)g.create();
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+        g2d.drawImage(bullet, (int)p.getX() - Grid.POSITION_SIZE / 4, (int)p.getY() - Grid.POSITION_SIZE / 4, (int)getPosition().getWidth() / 2, (int)getPosition().getHeight() / 2, null);
+    }
+    
+    //Postcondition - Checks if the Bullet and Enemy are in the same position, meaning they have collided
+    public boolean hasCollided(Enemy e)
+    {
+        if(e.getPosition().contains(p))
+            return true;
+        return false;
+    }
+    
+    //Postcondition - Moves the Bullet 12 pixels towards the Enemy based on the slope between the Bullet and Enemy
     public void move()
     {
         int slopeX = (int)(e.getXCoord() + Grid.POSITION_SIZE / 2 - p.getX());
@@ -85,23 +108,15 @@ public class Bullet extends Entity
         }
     }
     
-    public void draw(Graphics g)
+    //Postcondition - Moves the X-Coordinate of the Bullet by num pixels
+    public void moveX(int num)
     {
-        Graphics2D g2d = (Graphics2D)g.create();
-        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
-        g2d.drawImage(bullet, (int)p.getX() - Grid.POSITION_SIZE / 4, (int)p.getY() - Grid.POSITION_SIZE / 4, (int)getPosition().getWidth() / 2, (int)getPosition().getHeight() / 2, null);
+       p.translate(num, 0);
     }
-    
-    public boolean hasCollided(Enemy e)
+
+    //Postcondition - Moves the Y-Coordinate of the Bullet by num pixels
+    public void moveY(int num)
     {
-        if(e.getPosition().contains(p))
-            return true;
-        return false;
-    }
-    
-    public void damageEnemy(Enemy enemy)
-    {
-        enemy.changeHealth(damage);
-        changeGameState();
+       p.translate(0, num);
     }
 }
