b9bf088750d3f228b384af4d05f64a0163b003ac
jonjack25 <jonjack25@users.noreply.github.com>
2016-06-19 14:37:17 -0400

    Update Sound.java

--- Sound.java
+++ Sound.java
@@ -1,19 +1,27 @@
+//Sound - The Sound Class takes in a string and plays a .wav file from the resources folder
+
 import java.applet.*;
-import javax.sound.sampled.Clip;
 
 public class Sound
 {
+   //Private Fields
    private AudioClip music;
+   
+   //Constructors
    public Sound(String s)
    {
        music = Applet.newAudioClip(Sound.class.getResource(s));
    }
    
+   //Methods
+   
+   //Postcondition - Loops the specified AudioClip
    public void start()
    {
        music.loop();
    }
    
+   //Postcondition - Stops the specified AudioClip
    public void stop()
    {
        music.stop();
